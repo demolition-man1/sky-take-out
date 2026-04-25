feac0c3a289a586cb004395c48980e2c894e12cb
demolition man <o3464237004@gmail.com>
2026-04-25 22:22:56 +0800

    新增添加、查看购物车业务

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -28,24 +28,62 @@ public interface SetmealMapper {
      */
     @Select("select count(id) from setmeal where category_id = #{categoryId}")
     Integer countByCategoryId(Long id);
+    /**
+     * 插入套餐数据
+     * @param setmealDTO
+     */
     @AutoFill(value = OperationType.UPDATE)
     void update(SetmealDTO setmealDTO);
-
+    /**
+     * 分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
     Page<SetmealVO> pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
-
+    /**
+     * 修改套餐的起售停售状态
+     * @param setmeal
+     */
     void updateStatus(Setmeal setmeal);
-
+    /**
+     * 批量删除套餐
+     * @param ids
+     */
     void deleteBatch(List<Long> ids);
+    /**
+     * 根据id查询套餐和套餐关联的菜品数据
+     * @param ids
+     * @return
+     */
 
     List<Setmeal> getSetmealByIds(List<Long> ids);
+    /**
+     * 新增套餐
+     * @param setmeal
+     */
     @AutoFill(value = OperationType.INSERT)
     @Insert("insert into setmeal (name, category_id, price, status, description, image, create_time, update_time, create_user, update_user) " +
             "values (#{name}, #{categoryId}, #{price}, #{status}, #{description}, #{image}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
     void insert(Setmeal setmeal);
 //@Select("select * from setmeal where category_id = #{categoryId} and status = 1")
   //  List<Setmeal> getSetmealByCategoryId(Integer categoryId);
-
+    /**
+     * 根据套餐id查询包含的菜品列表
+     * @param setmealId
+     * @return
+     */
     List<DishItemVO> getDishBySetmealId(Integer setmealId);
-
+    /**
+     * 根据条件查询套餐数据
+     * @param setmeal
+     * @return
+     */
     List<Setmeal> list(Setmeal setmeal);
+    /**
+     * 根据id查询套餐
+     * @param id
+     * @return
+     */
+    @Select("select * from setmeal where id = #{id}")
+    Setmeal getById(Long id);
 }
---
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -0,0 +1,23 @@
+package com.sky.mapper;
+
+import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Update;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface ShoppingCartMapper {
+/**
+     * 动态条件查询
+     * @param shoppingCart
+     * @return
+     */
+    List<ShoppingCart> list(ShoppingCart shoppingCart);
+@Update("uodate shopping_cart set number = #{number} where id = #{id}")
+    void updateNumberById(ShoppingCart shoppingCart);
+@Insert("insert into shopping_cart (name, image, dish_id, setmeal_id, dish_flavor, number, amount, create_time)" +
+        " values (#{name}, #{image}, #{dishId}, #{setmealId}, #{dishFlavor}, #{number}, #{amount}, #{createTime})")
+    void insert(ShoppingCart shoppingCart);
+}
